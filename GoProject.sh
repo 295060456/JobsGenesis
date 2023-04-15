7643aa2ee8cfd436ee3eb1ebef93e0bc8189c297
jobs <jobs@gmail.com>
2023-04-16 04:20:31 +0700

    no message

--- GoProject.sh
+++ GoProject.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# 定义一些基本的函数
+function defaultInput() {
+    result=${1}
+    if [${1} = $""]
+    then
+        # 什么都不输入，即默认值
+        result=$'Test'
+    fi  
+}
+
 # 📌定位到桌面
 cd Desktop/
 # 先定义项目文件夹
@@ -7,13 +17,17 @@ printf '【自动化前置部署Go项目】\n\n'
 # 读取键盘输入，回车结束监听记录
 read -p "请输入项目文件夹名字:" folder_name
 
+<<'COMMENT'
 if ["$folder_name" = ""]
 then
     # 什么都不输入，即默认值
     folder_name="Test"
 fi
+COMMENT
 
-# echo "The value of var is ${folder_name}"
+defaultInput $folder_name
+folder_name=${result}
+# echo "The value of var is ${result}"
 
 mkdir ${folder_name}
 echo -n "已经在桌面建立一个名为:${folder_name}的文件夹，并打开他"
@@ -21,6 +35,10 @@ open ${folder_name}
 # 📌定位到刚才创立的项目文件夹
 cd ${folder_name}
 # 用mod管理项目
+<<'COMMENT'
+go.mod文件一旦创建后，它的内容将会被go toolchain全面掌控。
+go toolchain会在各类命令执行时，比如go get、go build、go mod等修改和维护go.mod文件。
+COMMENT
 go mod init gone
 
 # 用Visual Studio Code作为IDE编辑代码[可以放在最后]
@@ -30,16 +48,20 @@ code .
 echo "请输入新建文件名字，以回车结束\n"
 read -p "如果什么都没有输入，那么启用默认文件名：Test" file_name
 
+<<'COMMENT'
 if ["$file_name" = ""]
 then
     # 什么都不输入，即默认值
     file_name="Test"
 fi
+COMMENT
 
-# echo "The value of var is ${file_name}"
+defaultInput $file_name
+file_name=${result}
+# echo "The value of var is ${result}"
 
 # 新建一个文件，命名为Test
-$file_name=$file_name$".go"
+file_name=$file_name$".go"
 Touch $file_name
 
 # 写内容到文件
