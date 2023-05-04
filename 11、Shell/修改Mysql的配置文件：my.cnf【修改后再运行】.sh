67f3249fa9c47e80ed1a55f8473ccdd6fa66c453
jobs <jobs@gmail.com>
2023-05-04 19:05:48 +0700

    no message

--- 11、Shell/修改Mysql的配置文件：my.cnf【修改后再运行】.sh
+++ 11、Shell/修改Mysql的配置文件：my.cnf【修改后再运行】.sh
@@ -9,10 +9,10 @@ brew list mysql
 # 不出意外，会对外输出 /opt/homebrew/Cellar/mysql
 mysql --version
 
-read -p "请输入本机的 Mysql 版本号，以回车结束。默认8.0.33:" mysqlVersion
-if [[ $mysqlVersion = "" ]];then
-    mysqlVersion="8.0.33"
-fi
+# 字符串截取 mysql 的版本号
+mysqlVersion=`mysql --version`
+# mysql  Ver 8.0.33 for macos13.3 on arm64 (Homebrew)
+echo $mysqlVersion | awk -F 'Ver ' '{ print $2 }' | awk -F ' for' '{ print $1 }'
 fileCopy_fullname=$"/opt/homebrew/Cellar/mysql/"${mysqlVersion}"/.bottle/etc/my.cnf"
 echo "fileCopy_fullname:"$fileCopy_fullname
 
